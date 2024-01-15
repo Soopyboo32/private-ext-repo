ec7de1e7630a38bbcda99e2aa0451d55befa38d9
Soopyboo32 <49228220+Soopyboo32@users.noreply.github.com>
2024-01-15 14:23:17 +0800

    asd

--- update.js
+++ update.js
@@ -5,4 +5,5 @@ data[0].code = outmeta.code
 data[0].version = outmeta.versionName
 data[0].apk = outmeta.outputFile.replace("-release", "")
 fs.writeFileSync("./index.min.json", JSON.stringify(data))
+fs.writeFileSync("./index.json", JSON.stringify(data, undefined, 2))
 fs.renameSync("./apk/"+outmeta.outputFile, "./apk/"+data[0].apk)
