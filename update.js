c0522982618f8458407eac4360736bd1e9e17579
Soopyboo32 <49228220+Soopyboo32@users.noreply.github.com>
2024-01-15 14:23:50 +0800

    asd

--- update.js
+++ update.js
@@ -1,7 +1,7 @@
 const fs = require("fs")
 let outmeta = JSON.parse(fs.readFileSync("./apk/output-metadata.json")).elements[0]
 let data = JSON.parse(fs.readFileSync("./index.json"))
-data[0].code = outmeta.code
+data[0].code = outmeta.versionCode
 data[0].version = outmeta.versionName
 data[0].apk = outmeta.outputFile.replace("-release", "")
 fs.writeFileSync("./index.min.json", JSON.stringify(data))
